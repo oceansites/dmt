ee5552f0fd80e8cce2a8b25c63efcd0718337a15
Mike McCann <MBARIMike@gmail.com>
2016-05-01 08:05:20 +0000

    Add command line arguments that are similar to those used by compliance-checker

--- utilities/compliance_report.py
+++ utilities/compliance_report.py
@@ -13,6 +13,7 @@ import sys
 sys.path.insert(0, '/home/vagrant/dev/thredds_crawler')
 sys.path.insert(0, '/home/vagrant/dev/compliance-checker')
 
+import argparse
 from thredds_crawler.crawl import Crawl
 from compliance_checker.runner import ComplianceChecker, CheckSuite
 
@@ -20,16 +21,59 @@ from compliance_checker.runner import ComplianceChecker, CheckSuite
 check_suite = CheckSuite()
 check_suite.load_all_available_checkers()
 
-site_url = 'http://dods.ndbc.noaa.gov/thredds/catalog/oceansites/DATA/MBARI/catalog.xml'
+def main(args):
+    for cat in args.catalog_url:
+        select_str = ''
+        if args.pattern:
+            select_str = r'.*{}.*'.format(args.pattern)
 
-# Loop through OPeNDAP URLS for files from the GDAC
-##c = Crawl(site_url, select=[".*.nc$"], debug=True)
-c = Crawl(site_url, select=[".*OS_MBARI-M2_20100402_R_TS.nc$"], debug=True)
-for url in (s.get("url") for d in c.datasets 
-                                for s in d.services 
-                                    if s.get("service").lower() == "opendap"):
+        if args.verbose:
+            c = Crawl(cat, select=[select_str], debug=True)
+        else:
+            c = Crawl(cat, select=[select_str], debug=False)
 
-    url = 'http://dods.ndbc.noaa.gov/thredds/dodsC/oceansites/DATA/MBARI/OS_MBARI-M0_20040604_R_TS.nc'
-    # Send the compliance report to stdout
-    return_value, errors = ComplianceChecker.run_checker(
-                                url, ['cf', 'acdd'], None, 'normal', '-', 'text')
+        for url in (s.get("url") for d in c.datasets 
+                                        for s in d.services 
+                                            if s.get("service").lower() == "opendap"):
+
+            # Send the compliance report to stdout
+            return_value, errors = ComplianceChecker.run_checker(
+                                        url, ['cf', 'acdd'], True, 'normal', '-', 'text')
+
+def parse_command_line():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--pattern', '-p', '--pattern=', '-p=', 
+                        help="Select text for thredds_crawler: 'select=[.*<pattern>.*]'")
+    parser.add_argument('--test', '-t', '--test=', '-t=', default=('acdd',),
+                        nargs='+',
+                        choices=sorted(check_suite.checkers.keys()),
+                        help="Select the Checks you want to perform.  Defaults to 'acdd' if unspecified")
+
+    parser.add_argument('--criteria', '-c',
+                        help="Define the criteria for the checks.  Either Strict, Normal, or Lenient.  Defaults to Normal.",
+                        nargs='?', default='normal',
+                        choices = ['lenient', 'normal', 'strict'])
+
+    parser.add_argument('--verbose', '-v',
+                        help="Increase output. May be specified up to three times.",
+                        action="count",
+                        default=0)
+
+    parser.add_argument('-f', '--format', default='text',
+                        choices=['text', 'html', 'json'], help='Output format')
+    parser.add_argument('-o', '--output', default='-', action='store',
+                        help='Output filename')
+    parser.add_argument('-V', '--version', action='store_true',
+                        help='Display the IOOS Compliance Checker version information.')
+    parser.add_argument('catalog_url', nargs='*',
+                        help="The THREDDS Catalog URL ending in '.xml'.")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_command_line()
+    main(args)
+
